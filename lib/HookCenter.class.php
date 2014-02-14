f4c459f913d3da730f94cb1b5cdeb1f25886ed5e
Rasmus Fuhse <fuhse@data-quest.de>
2014-02-14 10:52:04 +0100

    make getDescription static and rename it to getHookDescription to avoid conflicts

--- lib/HookCenter.class.php
+++ lib/HookCenter.class.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * An interface for all hookclasses that should be handled by the HookCenter.
+ * They need a static public method getHookDescription to tell how the hookclass
+ * should be used. A derived hookclass can be very simple like
+ *     class BulletinBoardQuoteHook extends ArrayObject implements Hook 
+ *     {
+ *         static public function getHookDescription() 
+ *         {
+ *             return "Just put strings in the array that are randomly displayed 
+ *                      in the bulletin board as famous quotes.";
+ *         }
+ *     }
+ * Or they could be very complex with lots of methods and attributes. It's your 
+ * choice. But please write documentation so that everyone else knows how to use
+ * the hook.
+ * 
+ * As a convention derived classes should be named with "Hook" as the last four
+ * letters of the name.
+ */
 interface Hook 
 {
     /**
@@ -10,7 +29,7 @@ interface Hook
      * So this method more or less returns a documentation of the hook and all
      * its circumstances. Don't be afraid to write some few lines more. That's okay.
      */
-    public function getDescription();
+    static public function getHookDescription();
 }
 
 class ActionNavigationHook extends Navigation implements Hook {
@@ -35,7 +54,7 @@ class ActionNavigationHook extends Navigation implements Hook {
         return $this->postvars;
     }
     
-    public function getDescription() 
+    static public function getHookDescription() 
     {
         return "
             This hook is often called in lists at which you can see some icons
@@ -59,11 +78,53 @@ class ActionNavigationHook extends Navigation implements Hook {
 
 class DisplayOnlineUserActionHook extends ActionNavigationHook {}
 
+/**
+ * Class to be used to register and run hooks in Stud.IP. The difference to
+ * NotificationCenter is that the HookCenter doesn't handle simple events but
+ * it serves objects of hookclasses the registered function can interact with.
+ * 
+ * So if you just want to tell that you created a user, just fire the event with
+ * the NotificationCenter, but if you want to display a user and want to give 
+ * plugins or any other code the ability to add links or input fields or whatever
+ * you should use the HookCenter. The used hookclass gathers the icons, inputfields
+ * or whatever you want and you can use the object of type hookclass to display
+ * these information to the user-page.
+ * 
+ * Example for running a hook:
+ *     $actions = new DisplayOnlineUserActionHook("onlinelist");
+ *     $actions = HookCenter::run("DisplayOnlineUserActionHook", $actions);
+ *     foreach ($actions->getSubNavigation() as $action) {
+ *         echo '<a href="'.URLHelper::getLink($action->getURL()).'">'.htmlReady($action->getTitle()).'</a> ';
+ *     }
+ * 
+ * Example for registering a callback to a hook (should be run before the the hook is run):
+ *     HookCenter::register("DisplayOnlineUserActionHook", function ($navigation) {
+ *         $nav = new DisplayOnlineUserActionHook(_("Nachricht verfassen"), URLHelper::getURL("sms_send.php"));
+ *         $nav->setImage(Assets::image_path("icons/16/blue/mail.png"), array('title' => _("Nachricht verfassen")));
+ *         $navigation->addSubNavigation("messaging", $nav);
+ *     });
+ * 
+ * You can see that the hookclass is the mediator between the registered function
+ * and the code that has called the hook. To know as a programmer what exactly the
+ * code calling the hook is expecting, you only need to know about the hookclass.
+ * Just have a look at the class, at the documentation of the class and/or at 
+ * the output of the static method #getHookDescription() of the hookclass that 
+ * usually describes what the hook is doing and how the registered function can 
+ * interact with the hookclass.
+ */
 class HookCenter 
 {
     static protected $hooks = array(); //the hooks and registered callables
     
-    static public function register($hookclass, $callable) 
+    /**
+     * Registers a callback function for the case that the given hook is run.
+     * @param string $hookclass : name of the hookclass is the name of the hook itself.
+     * @param callable $callable : your callback. The first argument of the 
+     * callback-function will be an instance of type $hookclass your callback
+     * receives from the HookCenter when the hook is run. See documentation of
+     * the $hookclass to know how your callback can interact with the hook.
+     */
+    static public function register($hookclass, callable $callable) 
     {
         self::$hooks[$hookclass][] = $callable;
     }
@@ -91,7 +152,7 @@ class HookCenter
      * all registered functions and can contain more information or a different 
      * state than before.
      */
-    static public function run($hookclass = null, $instance = null) 
+    static public function run($hookclass, $instance = null) 
     {
         if ($instance === null or !is_a($instance, $hookclass)) {
             $instance = new $hookclass();
