f9d074dcf936850ad442e80ae99f95fd2a64da56
Rasmus Fuhse <krasmus@gmail.com>
2014-02-24 07:00:59 +0100

    change comment

--- lib/ActionNavigationHook.class.php
+++ lib/ActionNavigationHook.class.php
@@ -31,21 +31,21 @@ class ActionNavigationHook extends Navigation implements Hook {
     static public function getHookDescription() 
     {
         return "
-            This hook is often called in lists at which you can see some icons
-            on the right side of the list that are representing actions or links
-            to sites in Stud.IP. Just like at the online-page or the participants
-            page in the course.
-            
-            You can think of such an icon as a subnavigation of this ActionNavigation-
-            object. So if you want to add an icon/action to the list, just call 
-            #addSubNavigation (derived from Navigation class) to add your own 
-            ActionNavigation objects. Also you can delete existing subnavigation
-            objects or rearrange them - just like in casual navigation.
-            
-            If you want to add an action that corresponding to a POST-request and
-            not a simple GET-request, you can call #addPostVariables(name, value)
-            and the icon should be wrapped in a <form>-tag with hidden input 
-            fields and an XSRF-token, so your action is a valid POST-request.
+This hook is often called in lists at which you can see some icons
+on the right side of the list that are representing actions or links
+to sites in Stud.IP. Just like at the online-page or the participants
+page in the course.
+
+You can think of such an icon as a subnavigation of this ActionNavigation-
+object. So if you want to add an icon/action to the list, just call 
+#addSubNavigation (derived from Navigation class) to add your own 
+ActionNavigation objects. Also you can delete existing subnavigation
+objects or rearrange them - just like in casual navigation.
+
+If you want to add an action that corresponding to a POST-request and
+not a simple GET-request, you can call #addPostVariables(name, value)
+and the icon should be wrapped in a form-tag with hidden input 
+fields and an XSRF-token, so your action is a valid POST-request.
         ";
     }
 }
